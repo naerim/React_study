028f30c496e5c51a65fa8579e1cf9157fc0af026
naerim <naerim1119@gmail.com>
2020-07-08 02:43:14 +0900

    Create TodoList using redux

--- react-redux_study/src/App.js
+++ react-redux_study/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import CounterContainer from "./containers/CounterContainer";
-import TodosContainer from "./containers/TodosContainer";
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 const App = () => {
   return (
     <div>
       <CounterContainer />
-      <hr/>
-      <TodosContainer/>
+      <hr />
+      <TodosContainer />
     </div>
   );
 };
---
+++ todo_redux/src/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components";
+import Todos from "./component/Todos";
+
+function App() {
+  return (
+    <TodoTemplate>
+      <Title>TODO LIST</Title>
+      <Content>
+        <ThisWeek>
+          <Name>This Week</Name>
+          <Todos name="week" />
+        </ThisWeek>
+        <ThisMonth>
+          <Name>This Month</Name>
+          <Todos />
+        </ThisMonth>
+      </Content>
+    </TodoTemplate>
+  );
+}
+
+const TodoTemplate = styled.div`
+  width: 512px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 6rem;
+`;
+
+const Title = styled.div`
+  background: seagreen;
+  color: white;
+  height: 4rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+`;
+
+const Name = styled.div`
+  height: 2rem;
+  color: white;
+  background: olivedrab;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ThisWeek = styled.div`
+  width: 100%;
+  background: yellow;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ThisMonth = styled.div`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  float: center;
+`;
+
+export default App;
