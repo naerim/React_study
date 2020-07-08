2c36c1ee966eb61ddaaac1c6feefa5236d06cb5a
naerim <naerim1119@gmail.com>
2020-07-09 02:15:22 +0900

    Use todosWeek at combineReducers

--- todo_redux/src/module/index.js
+++ todo_redux/src/module/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
-
+import todosWeek from "./todosWeek";
 import todos from "./todos";
 
 const rootReducer = combineReducers({
+  todosWeek,
   todos,
 });
 
