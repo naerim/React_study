7147092cdea47e5310814c628332977e6942002b
naerim <naerim1119@gmail.com>
2020-07-10 11:50:30 +0900

    Use persistReducer

--- todo_redux/src/module/index.js
+++ todo_redux/src/module/index.js
@@ -1,10 +1,17 @@
 import { combineReducers } from "redux";
 import todosWeek from "./todosWeek";
 import todos from "./todos";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 const rootReducer = combineReducers({
   todosWeek,
   todos,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
