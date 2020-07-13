5f17c28429a122b395ff7a9927b810a3afcb6fe5
naerim <naerim1119@gmail.com>
2020-07-13 23:28:00 +0900

    [FIX] Simplify Todos

--- todo_redux/src/component/Todos.js
+++ todo_redux/src/component/Todos.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import useActions from "../lib/useActions";
-import { changeInput, insert, remove, toggle } from "../module/todos";
 import {
   changeInputWeek,
   insertWeek,
-  removeWeek,
   toggleWeek,
-} from "../module/todosWeek";
+  removeWeek,
+  changeInputMonth,
+  insertMonth,
+  toggleMonth,
+  removeMonth,
+} from "../module/todos";
+import { useSelector } from "react-redux";
+import useActions from "../lib/useActions";
 
 const TodoItem = ({ todo, onToggle, onRemove }) => {
   return (
@@ -26,20 +29,14 @@ const TodoItem = ({ todo, onToggle, onRemove }) => {
   );
 };
 
-const Todos = (props) => {
-  const { input, todos } = useSelector(({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos,
-  }));
-
-  const { inputWeek, todosWeek } = useSelector(({ todosWeek }) => ({
-    inputWeek: todosWeek.inputWeek,
-    todosWeek: todosWeek.todosWeek,
-  }));
-
-  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
-    [changeInput, insert, toggle, remove],
-    []
+const Todos = ({ week }) => {
+  const { inputWeek, inputMonth, todosWeek, todosMonth } = useSelector(
+    ({ todos }) => ({
+      inputWeek: todos.inputWeek,
+      inputMonth: todos.inputMonth,
+      todosWeek: todos.todosWeek,
+      todosMonth: todos.todosMonth,
+    })
   );
 
   const [
@@ -47,66 +44,59 @@ const Todos = (props) => {
     onInsertWeek,
     onToggleWeek,
     onRemoveWeek,
-  ] = useActions([changeInputWeek, insertWeek, toggleWeek, removeWeek], []);
+    onChangeInputMonth,
+    onInsertMonth,
+    onToggleMonth,
+    onRemoveMonth,
+  ] = useActions(
+    [
+      changeInputWeek,
+      insertWeek,
+      toggleWeek,
+      removeWeek,
+      changeInputMonth,
+      insertMonth,
+      toggleMonth,
+      removeMonth,
+    ],
+    []
+  );
+
+  // week에 따라 구분
+  const input = week ? inputWeek : inputMonth;
+  const todos = week ? todosWeek : todosMonth;
+  const onInsert = week ? onInsertWeek : onInsertMonth;
+  const onChangeInput = week ? onChangeInputWeek : onChangeInputMonth;
+  const onRemove = week ? onRemoveWeek : onRemoveMonth;
+  const onToggle = week ? onToggleWeek : onToggleMonth;
 
   const onSubmit = (e) => {
     e.preventDefault();
     onInsert(input);
-    onChangeInput(""); // 등록 후 인풋 포기화
+    onChangeInput(""); // 등록 후 인풋 초기화
   };
-
+  
   const onChange = (e) => onChangeInput(e.target.value);
-
-  const onSubmitWeek = (e) => {
-    e.preventDefault();
-    onInsertWeek(inputWeek);
-    onChangeInputWeek(""); // 등록 후 인풋 포기화
-  };
-
-  const onChangeWeek = (e) => onChangeInputWeek(e.target.value);
 
   return (
     <div>
-      {props.name === "week" ? (
-        <form onSubmit={onSubmitWeek}>
-          <input value={inputWeek} onChange={onChangeWeek} />
-          <button type="submit">등록</button>
-        </form>
-      ) : (
-        <form onSubmit={onSubmit}>
-          <input value={input} onChange={onChange} />
-          <button type="submit">등록</button>
-        </form>
-      )}
-      {props.name === "week" ? (
-        <div>
-          {todosWeek.map((todo) => (
-            <TodoItem
-              todo={todo}
-              key={todo.id}
-              onToggle={onToggleWeek}
-              onRemove={onRemoveWeek}
-              onChangeInput={onChangeInputWeek}
-              onInsert={onInsertWeek}
-            />
-          ))}
-        </div>
-      ) : (
-        <div>
-          {todos.map((todo) => (
-            <TodoItem
-              todo={todo}
-              key={todo.id}
-              onToggle={onToggle}
-              onRemove={onRemove}
-              onChangeInput={onChangeInput}
-              onInsert={onInsert}
-            />
-          ))}
-        </div>
-      )}
+      <form onSubmit={onSubmit}>
+        <input value={input} onChange={onChange} />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            onToggle={onToggle}
+            onRemove={onRemove}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default Todos;
+export default React.memo(Todos);
+
