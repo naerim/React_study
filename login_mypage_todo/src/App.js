a2fe5f1cac006cc1fa881a76fe1df6c047680418
naerim <naerim1119@gmail.com>
2020-07-20 02:50:03 +0900

    [ADD] BrowserRouter as Router, Route, Switch

--- login_mypage_todo/src/App.js
+++ login_mypage_todo/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./components/main";
 import Login from "./components/login";
 import Todos from "./components/todos";
 import Category from "./components/category";
+import MyPage from "./components/myPage";
 
 const App = () => {
   return (
     <div>
-      <Category />
-      <Todos />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/myPage" component={MyPage} />
+          <Route exact path="/todos" component={Todos} />
+        </Switch>
+      </Router>
     </div>
   );
 };
