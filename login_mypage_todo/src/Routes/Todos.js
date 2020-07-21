17aa2504bb1defa28fe7527a7aca473fb8c647a2
naerim <naerim1119@gmail.com>
2020-07-22 00:48:51 +0900

    [FIX] Todos

--- login_mypage_todo/src/Routes/Todos.js
+++ login_mypage_todo/src/Routes/Todos.js
@@ -1,29 +1,65 @@
 import React from "react";
 import styled from "styled-components";
 import Category from "../Components/category";
-const TodoItem = () => {
+import { changeInput, insert, toggle, remove } from "../Modules/todos";
+import { useSelector } from "react-redux";
+import useActions from "../Lib/useActions";
+
+const TodoItem = ({ todo, onToggle, onRemove }) => {
   return (
-    <div>
-      <div>할 일</div>
-    </div>
+    <TodoContainer>
+      <input
+        type="checkbox"
+        onClick={() => onToggle(todo.id)}
+        checked={todo.done}
+        readOnly={true}
+      />
+      <span style={{ textDecoration: todo.done ? "line-through" : "none" }}>
+        {todo.text}
+      </span>
+      <button onClick={() => onRemove(todo.id)}>삭제</button>
+    </TodoContainer>
   );
 };
 
 const Todos = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onInsert(input);
+    onChangeInput("");
+  };
+
+  const onChange = (e) => onChangeInput(e.target.value);
+
   return (
     <div>
       <Category />
       <TodoSection>
         <Title>--TodoList--</Title>
-        <Contents>
-          <TodoForm>
-            <input />
-            <button type="submit">등록</button>
-          </TodoForm>
-          <TodoContainer>
-            <TodoItem />
-          </TodoContainer>
-        </Contents>
+        <TodoForm onSubmit={onSubmit}>
+          <input value={input} onChange={onChange} />
+          <button type="submit">등록</button>
+        </TodoForm>
+        <Content>
+          {todos.map((todo) => (
+            <TodoItem
+              todo={todo}
+              key={todo.id}
+              onToggle={onToggle}
+              onRemove={onRemove}
+            />
+          ))}
+        </Content>
       </TodoSection>
     </div>
   );
@@ -51,17 +87,17 @@ const TodoForm = styled.form`
   align-items: center;
   justify-content: center;
 `;
-
-const Contents = styled.div`
-  width: 100%;
+const Content = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 const TodoContainer = styled.div`
-  display: flex;
   width: 100%;
+  display: flex;
   align-items: center;
   justify-content: center;
+  margin-top: 1%;
 `;
 
 export default Todos;
