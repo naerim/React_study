2813c87a106a4fee843ec235cbe3db90913285bb
naerim <naerim1119@gmail.com>
2020-07-23 23:54:11 +0900

    [ADD] user at rootReducer

--- login_mypage_todo/src/Modules/index.js
+++ login_mypage_todo/src/Modules/index.js
@@ -1,9 +1,9 @@
 import {combineReducers} from "redux";
 import todos from './todos';
-import login from "./login";
+import user from "./user";
 
 const rootReducer = combineReducers({
-    todos,login
+    todos,user
 })
 
 export default rootReducer;
