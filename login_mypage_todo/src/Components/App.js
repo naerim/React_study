4a0fee240df7addf3f3f7cd4d709456cc95e8015
naerim <naerim1119@gmail.com>
2020-07-23 21:18:22 +0900

    [ADD] 404 page

--- login_mypage_todo/src/Components/App.js
+++ login_mypage_todo/src/Components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Main from "../Routes/Main";
 import Login from "../Routes/Login";
 import MyPage from "../Routes/MyPage";
 import Todos from "../Routes/Todos";
+import NotFound from "../Routes/NotFound";
 
 const App = () => {
   return (
@@ -14,6 +20,8 @@ const App = () => {
           <Route exact path="/login" component={Login} />
           <Route exact path="/myPage" component={MyPage} />
           <Route exact path="/todos" component={Todos} />
+          <Route path="/404" component={NotFound} />
+          <Redirect from="*" to="/404" />
         </Switch>
       </Router>
     </div>
