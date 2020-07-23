333473633bcf49f5058898ee5da67431fb15d47c
naerim <naerim1119@gmail.com>
2020-07-23 21:22:06 +0900

    [DEL] UseInputs, login, myPage

--- login_mypage_todo/src/Hooks/UseInputs.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-const UseInputs = () => {
-  const [value, setValue] = useState("");
-  const Input = (e) => setValue(e.target.value);
-  return [value, Input];
-};
-
-export default UseInputs;
