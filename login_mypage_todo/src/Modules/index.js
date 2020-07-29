aa56035a181872364b2ce1ecee88566683ec95c1
naerim <naerim1119@gmail.com>
2020-07-29 23:09:09 +0900

    [ADD] redux-persist

--- login_mypage_todo/src/Modules/index.js
+++ login_mypage_todo/src/Modules/index.js
@@ -1,9 +1,18 @@
-import {combineReducers} from "redux";
-import todos from './todos';
+import { combineReducers } from "redux";
+import todos from "./todos";
 import user from "./user";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 const rootReducer = combineReducers({
-    todos,user
-})
+  todos,
+  user,
+});
 
-export default rootReducer;
+
+export default persistReducer(persistConfig, rootReducer);
