153a856be438cc8078fdf62cac6231c3cc764cb9
naerim <naerim1119@gmail.com>
2020-08-04 20:54:09 +0900

    [ADD] createRequestThunk

--- redux-middleware/src/modules/webRequest.js
+++ redux-middleware/src/modules/webRequest.js
@@ -1,5 +1,6 @@
 import produce from "immer";
 import * as api from "../lib/api";
+import createRequestThunk from "../lib/createRequestThunk";
 
 // 액션 타입, 한 요청당 세 개
 const GET_POST = "GET_POST";
@@ -11,41 +12,8 @@ const GET_USERS_FAILURE = "GET_USERS_FAILURE";
 
 // thunk 함수 생성
 // thunk 함수 내부에서는 시작, 성공, 실패했을 때 다른 액션을 디스패치 한다.
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e;
-  }
-};
-
-export const getUsers = () => async (dispatch) => {
-  dispatch({ type: GET_USERS }); // 요청을 시작한 것을 알림
-  try {
-    const response = await api.getUsers();
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 // 초기 상태, 요청의 로딩 중 상태는 loading이라는 객체애서 관리
 const initialState = {
