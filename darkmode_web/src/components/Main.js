2420d581e6de3bc74326710f13119214cfa2cbab
naerim <naerim1119@gmail.com>
2022-03-11 12:21:06 +0900

    [Fix] styles

--- darkmode_web/src/components/Main.js
+++ darkmode_web/src/components/Main.js
@@ -1,7 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+import { useTheme } from "../context/themeProvider";
+import AppLayout from "./AppLayout";
 
 const Main = () => {
-  return <div>Main</div>;
+  const ThemeMode = useTheme();
+  const CurrentMode = ThemeMode[0] === "light" ? "🌝" : "🌚";
+
+  return (
+    <AppLayout>
+      <h2>
+        Welcome to {""}
+        <a
+          href="https://github.com/gparkkii/react_darkmode"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Darkmode!
+        </a>
+        <br />
+        <ColoredText>Current mode is {CurrentMode}</ColoredText>
+      </h2>
+      <a
+        href="https://github.com/gparkkii"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        All rights reserved by Ji Yeon Park
+      </a>
+    </AppLayout>
+  );
 };
 
 export default Main;
+
+const ColoredText = styled.span`
+  color: #e6b74a;
+`;
--- darkmode_web/src/components/Sub.js
+++ darkmode_web/src/components/Sub.js
@@ -1,7 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+import AppLayout from "./AppLayout";
+import { Link } from "react-router-dom";
+import { useTheme } from "../context/themeProvider";
 
 const Sub = () => {
-  return <div>Sub</div>;
+  const ThemeMode = useTheme();
+  return (
+    <AppLayout>
+      <h2> ...SubPage </h2>
+      <Link to="/">
+        <StyledButton theme={ThemeMode[0]}>Go back to Home</StyledButton>
+      </Link>
+    </AppLayout>
+  );
 };
+
+const StyledButton = styled.button`
+  width: 240px;
+  height: 56px;
+  border-radius: 4px;
+  border: ${(props) =>
+    props.theme === "light" ? "1px solid #31302E" : "1px solid #bbb"};
+  color: ${(props) => (props.theme === "light" ? "#31302E" : "#bbb")};
+`;
 
 export default Sub;
